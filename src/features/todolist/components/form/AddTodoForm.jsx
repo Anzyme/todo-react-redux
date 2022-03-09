7cbe692e3c0e7c98ad5407d2809d6ab3cd42386d
Anzyme <kamil.houhou@gmail.com>
2022-03-09 16:36:15 +0100

    Added feature to display todos count + disable submit button on empty title + reset input after todo add

--- src/features/todolist/components/form/AddTodoForm.jsx
+++ src/features/todolist/components/form/AddTodoForm.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTodo } from "../../../../redux/TodoSlice";
 
 export default function AddTodoForm() {
   const [value, setValue] = useState("");
+  const titleRef = useRef();
 
   const dispatch = useDispatch();
 
@@ -14,21 +15,23 @@ export default function AddTodoForm() {
         title: value,
       })
     );
+    setValue("");
   };
 
   return (
     <form onSubmit={onSubmit} className="form-inline mt-3 mb-3">
       <label className="sr-only">Name</label>
       <input
+        ref={titleRef}
         type="text"
         className="form-control mb-2 mr-sm-2"
-        placeholder="Add todo..."
+        placeholder="Ajouter une tâche..."
         value={value}
         onChange={(event) => setValue(event.target.value)}
       ></input>
 
-      <button type="submit" className="btn bg-blue-500 text-white mb-2">
-        Submit
+      <button disabled={!value} type="submit" className="btn bg-blue-500 text-white mb-2">
+        Ajouter
       </button>
     </form>
   );
--- src/redux/TodoSlice.js
+++ src/redux/TodoSlice.js
@@ -2,11 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const TodoSlice = createSlice({
   name: 'todos',
-  initialState: [
-    { id: 1, title: 'Todo 1', completed: false },
-    { id: 2, title: 'Todo 2', completed: false },
-    { id: 3, title: 'Todo 3', completed: true },
-  ],
+  initialState: [],
   reducers: {
     addTodo: (state, action) => {
       //Ajouter un todo
